fb60828b1f658068565a5f728d6f5effa7b88f71
Vishal Sharma <785vishal443@gmail.com>
2022-07-24 22:20:04 +0530

    Create BubbleSort.java

---
+++ BubbleSort.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class BubbleSort {
+
+    public  static void printMyArray(int array[]) {
+        for(int i=0;i<array.length;i++){
+            System.out.print(array[i]+ ",");
+        }
+    }
+    public static void main(String[] args) {
+        int array [] = {7,8,3,12,21,14,2,90,5,16,12,1};
+      
+        // Bubble Sort 
+        for(int i= 0 ;i<array.length;i++){ 
+          
+          // loop for comparing adjacent element
+            for(int j = 0; j<array.length-i-1;j++) {
+              // comparing if right side of current element is smaller is yes swap it
+                if(array[j]>array[j+1]){
+                    int temp = array[j];
+                    array[j]= array[j+1];
+                    array[j+1]= temp;
+                }
+            }
+        }
+        // System.out.println(Arrays.toString(array)); // online data type
+        printMyArray(array);
+    }
+}
+
