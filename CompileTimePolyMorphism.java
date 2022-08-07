e9f106f6f429cb5e7aa4dcccb22bc02ced4f6e25
Vishal Sharma <785vishal443@gmail.com>
2022-08-07 23:04:58 +0530

    Update CompileTimePolyMorphism.java

--- CompileTimePolyMorphism.java
+++ CompileTimePolyMorphism.java
@@ -1,47 +1,62 @@
- class Animal  {
-    String defaultFavourite = "User";
-    public void favouriteDish(){
-        System.out.println("Yet to know... ");
+
+class MethodOverloadingExample {
+
+    String defaultName = "User";
+    // printName with empty parameter
+    public void  printName() {
+        System.out.println("My name is:"+ " " + defaultName);
     }
-    Animal() {
-        System.out.println("Constructor");
+   // printName with one parameter
+    public void printName(String name){
+        System.out.println("My name is: " + name);
     }
- }
 
- class Dog extends Animal {
-    String defaultFavourite = "Vishal";
-    public void favouriteDish(){
-        System.out.println("Fish");
-}  
+ // printName functions with int data type with one parameter
+    public void printPrice(int  price){
+        System.out.println("My name is: " + price);
+    }
+ // printName functions with float data type with one parameter
+    public void printPrice(float  price){
+        System.out.println("My name is: " + price);
+    }
 
- }
 
- class Cat extends Animal  {
-    String defaultFavourite = "Vishal";
-    public void favouriteDish(){
-        System.out.println("Bone");
-}  
- }
+   // printName with two parameter
+    public void printName(String name , String lastName) {
+        System.out.println("My name is: " + name+ " " + lastName);
+    }
+ // printName with two parameter
+    public void printName(String name , String middleName ,String lastName) {
+        System.out.println("My name is: " + name+ " " + middleName+ " " +lastName);
+    }
 
-public class Overriding {
+    MethodOverloadingExample() {
+        System.out.println("I am constructor");
+    }
+    MethodOverloadingExample(String name) {
+        this.defaultName = name;
+    }
+}
+public class Polymorphism {
     public static void main(String[] args) {
-         Animal c = new Cat();
-         Dog d = new Dog();
-         d.favouriteDish();
-         c.favouriteDish();
-        //  d.defaultFavourite // this will give you the answer
-        //  c.favouriteDish // this will give you the error
+        // PolyMorphismh
+        // MethodOverloadingExample overloading1 = new MethodOverloadingExample();
+        MethodOverloadingExample overloading2 = new MethodOverloadingExample("Anonymous");
+        overloading2.printName();
+        overloading2.printName("Vishal");
+        overloading2.printName("vishal" , "sharma");
+        overloading2.printName("vishal" ,"joshi", "sharama");
+
+        
+
     }
 }
 
-// Power button laptop , remote , usb
+//  There should be atleast two method with same name to encounter method overloading  => static polyporphism
+//  there should be different parameter( number of parameter or type of parameter)
 
-// Dynamic data hiding
 
 
-// Can we overrride constructor ?
-
-// Can we overload constructor ?
 
 
 
